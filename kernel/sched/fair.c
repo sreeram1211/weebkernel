62bcda67e680450649eafb415853babc1c0255b6
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-02-08 16:43:45 +0530

    ANDROID: sched/fair: fix CPU selection for non latency sensitive tasks
    The Non latency sensitive tasks CPU selection targets for an active
    CPU in the little cluster. The shallowest c-state CPU is stored as
    a backup. However if all CPUs in the little cluster are idle, we pick
    an active CPU in the BIG cluster as the target CPU. This incorrect
    choice of the target CPU may not get corrected by the
    select_energy_cpu_idx() depending on the energy difference between
    previous CPU and target CPU.
    
    This can be fixed easily by maintaining the same variable that tracks
    maximum capacity of the traversed CPU for both idle and active CPUs.
    
    Change-Id: I3efb8bc82ff005383163921ef2bd39fcac4589ad
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6751,7 +6751,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				continue;
 
 			/*
-			 * Favor CPUs with smaller capacity for Non latency
+			 * Favor CPUs with smaller capacity for non latency
 			 * sensitive tasks.
 			 */
 			if (capacity_orig > target_capacity)
@@ -6784,10 +6784,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if (idle_cpu(i)) {
 				int idle_idx = idle_get_state_idx(cpu_rq(i));
 
-				/* Select idle CPU with lower cap_orig */
-				if (capacity_orig > best_idle_min_cap_orig)
-					continue;
-
 				/*
 				 * Skip CPUs in deeper idle state, but only
 				 * if they are also less energy efficient.
@@ -6798,8 +6794,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				    best_idle_cstate <= idle_idx)
 					continue;
 
-				/* Keep track of best idle CPU */
-				best_idle_min_cap_orig = capacity_orig;
+				target_capacity = capacity_orig;
 				best_idle_cstate = idle_idx;
 				best_idle_cpu = i;
 				continue;
