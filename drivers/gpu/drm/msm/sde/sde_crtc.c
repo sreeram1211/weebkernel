f97297216a72d9588357aa6cd6d3c073b69deee3
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-21 12:19:44 +0000

    drm: msm: sde: Boost CPU when a new frame is ready to be committed
    Seems like this is the correct place for this. crtc_commit goes high
    in CPU during UiBench invalidade test running top:
    
    CPU usage: 6.0% [crtc_commit:111]
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -20,6 +20,7 @@
 #include <linux/sort.h>
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
+#include <linux/cpu_input_boost.h>
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_crtc.h>
@@ -948,8 +949,12 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc)
 	if (unlikely(!sde_crtc->num_mixers))
 		return;
 
-
 	SDE_ATRACE_BEGIN("crtc_commit");
+
+	cpu_input_boost_kick();
+
+	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
+
 	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
 		if (encoder->crtc != crtc)
 			continue;
