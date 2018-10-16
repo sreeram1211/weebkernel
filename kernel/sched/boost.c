150282b891071cb2ff75e6454ae1aae80aafbbae
idkwhoiam322 <snkm1999@gmail.com>
2018-10-16 17:47:50 +0530

    cleanup after my own mess :)

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -25,10 +25,6 @@ unsigned int sysctl_sched_boost;
 static int boost_slot;
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int boost_slot;
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
-
 static bool verify_boost_params(int old_val, int new_val)
 {
 	/*
