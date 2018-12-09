89b46a69ddd46b03b8b7937f24c9779437a768b5
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    drm/msm: sde: boost only if last interaction was within 5000 ms
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -952,8 +952,12 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc)
 
 	SDE_ATRACE_BEGIN("crtc_commit");
 
-	cpu_general_boost_kick(150);
-	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+#ifdef CONFIG_CPU_INPUT_BOOST
+	if (time_before(jiffies, last_input_time + msecs_to_jiffies(5000))) {
+		cpu_general_boost_kick(150);
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+	}
+#endif
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 
